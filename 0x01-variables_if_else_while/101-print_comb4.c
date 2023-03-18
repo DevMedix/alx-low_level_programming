225992f732d2a02000c100b916cbf5ae0ae0de36
DevMedix <rawlingsmichael01@gmail.com>
2023-03-18 00:32:24 +0000

    C program that prints 3 combination of numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+	while (a <= 7)
+	{
+		int b = a + 1;
+
+		while(b <= 8)
+		{
+			int c = b + 1;
+			while(c <= 9)
+			{
+				putchar((a % 10) + '0');
+				putchar((b % 10) + '0');
+				putchar((c % 10) + '0');
+				if (a <= 7 || b <= 8 || c <= 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
