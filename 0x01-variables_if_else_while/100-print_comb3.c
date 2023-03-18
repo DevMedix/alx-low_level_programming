cd72df8f28cd571dfc74067c396642edc40df78d
DevMedix <rawlingsmichael01@gmail.com>
2023-03-18 00:21:10 +0000

    C file to print numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,35 +7,15 @@
  */
 int main(void)
 {
-	/*int a = 0;
-
-	while (a <= 8)
-	{
-		int b = a + 1;
-
-		while (b <= 9)
-		{
-			putchar((a % 10) + '0');
-			putchar((b % 10) + '0');
-			if (a <= 8 && b <= 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-			b++;
-		}
-		a++;
-	}*/
-
 	int a, b;
 
 	for (a = 0; a < 9; a++)
 	{
-		for (b = a +1; b < 10; b++)
+		for (b = a + 1; b < 10; b++)
 		{
 			putchar((a % 10) + '0');
 			putchar((b % 10) + '0');
-			if (a <= 8 && b <= 9)
+			if (a < 8 || b < 9)
 			{
 				putchar(',');
 				putchar(' ');
