c6467769037434c3c84c5b4847dd29edd83813d9
DevMedix <rawlingsmichael01@gmail.com>
2023-03-21 12:00:10 +0000

    C Program that prints all lowercase alphabet ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * print_alphabet_x10 - prints out all lowercase alphabet ten times
+ *
+ * Return: No return type present
+ */
 void print_alphabet_x10(void)
 {
 	int a = 48;
