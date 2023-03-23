6ef7aab9bdcc27da4cf2168afdce6e9ae4f51963
DevMedix <rawlingsmichael01@gmail.com>
2023-03-23 14:15:03 +0000

    Functions and nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - prints out only uppercase alphabet
+ * @c: alphabet to print out
+ *
+ * Return: 1 if c is uppercase, else 0
+ */
+ int _isupper(int c)
+{
+	if ((c >= 65) && (c <= 90))
+		return (1);
+	else
+		return (0);
+}
