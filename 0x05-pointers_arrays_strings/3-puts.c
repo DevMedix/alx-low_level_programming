70c46cd8b9c4f3e7b6d08182f5088614877ba52f
DevMedix <rawlingsmichael01@gmail.com>
2023-03-27 21:43:06 +0000

    a function that prints a string, followed by a new line, to stdout.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _puts - prints out a string
+ * _strlen - gets the length of a string
+ * @str: string tp print out
+ * @s: string to count
+ *
+ * Return: length
+ */
+int _strlen(char *s);
+void _puts(char *str)
+{
+	int i, length;
+
+	length = _strlen(str);
+	for (i = 0; i <= length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+int _strlen(char *s)
+{
+	int length;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
