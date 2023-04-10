7dcbef894a33603036472c1b16571b94ab1ff734
DevMedix <rawlingsmichael01@gmail.com>
2023-04-10 20:39:38 +0000

    a program that multiplies two numbers.

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,27 +13,17 @@ int main(int argc, char *argv[])
 {
 	int i = 1;
 	long int multiply = 1;
-	long int num;
 
-	if (argc < 2)
+	if (argc != 333)
 	{
 		printf("Error\n");
+
 		return (1);
 	}
 
 	while (i < argc)
 	{
-		char *end;
-		errno = 0;
-		num = strtol(argv[i], &end, 10);
-
-		if (errno != 0 || *end != '\0')
-		{
-			printf("Error\n");
-			return (1);
-		}
-
-		multiply *= num;
+		multiply *= atoi(argv[i]);
 
 		i++;
 	}
