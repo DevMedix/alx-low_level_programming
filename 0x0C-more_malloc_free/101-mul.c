26d5e2f799b62e4a86bfc3346e945d9d484f6539
DevMedix <rawlingsmichael01@gmail.com>
2023-04-14 00:37:54 +0000

    a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,73 +1,98 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
+#include <stdio.h>
+#include "main.h"
 /**
- * _positive_digit - checks for digits
- * @num: integer to check
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be checked
  *
- * Return: returns 1
+ * Return: 0 if a non-digit, else 1
  */
-int _positive_digit(char *num)
+int is_digit(char *s)
 {
 	int i = 0;
-	int len = strlen(num);
 
-	for (; i < len; i++)
+	while (s[i])
 	{
-		if (!isdigit(num[i]))
-		{
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		}
+		i++;
 	}
 	return (1);
 }
-
 /**
- * multiply - multiplies two numbers
- * @a: first number
- * @b: second number
+ * _strlen - prints the length of a string
+ * @s: string to print length
  *
- * Return: return the result of the multiplication
+ * Return: the length of the string
  */
-
-int multiply(char *a, char *b)
+int _strlen(char *s)
 {
-	return (atoi(a) * atoi(b));
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
 }
 
 /**
- * main - entry point
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: returns 0
+ * errors - handles errors for main
  */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+/**
+ * main - multiplies two positive numbers
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: always 0 (Success)
+ */
+
 int main(int argc, char *argv[])
 {
-	int result;
-	char *num1, *num2;
+	char *s1, *s2;
+	int len_s1, len_s2, len, i, array, digit1, digit2, *result, a = 0;
 
-	if (argc != 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len_s1 = _strlen(s1);
+	len_s2 = _strlen(s2);
+	len = len_s1 + len_s2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len_s1 + len_s2; i++)
+		result[i] = 0;
+	for (len_s1 = len_s1 - 1; len_s1 >= 0; len_s1--)
 	{
-		printf("Error\n");
-		exit(98);
+		digit1 = s1[len_s1] - '0';
+		array = 0;
+		for (len_s2 = _strlen(s2) - 1; len_s2 >= 0; len_s2--)
+		{
+			digit2 = s2[len_s2] - '0';
+			array += result[len_s1 + len_s2 + 1] + (digit1 * digit2);
+			result[len_s1 + len_s2 + 1] = array % 10;
+			array /= 10;
+		}
+		if (array > 0)
+			result[len_s1 + len_s2 + 1] += array;
 	}
-
-	num1 = argv[1];
-	num2 = argv[2];
-
-	if (!_positive_digit(num1) || !_positive_digit(num2))
+	for (i = 0; i < len - 1; i++)
 	{
-		printf("Error\n");
-		exit(98);
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-
-	result = multiply(num1, num2);
-
-	printf("%d\n", result);
-
+	if (!a)
+		printf("0");
+	_putchar(10);
+	free(result);
 	return (0);
 }
 
