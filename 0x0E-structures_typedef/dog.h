b787b43d7e66fbb7ef8e74f7ed3453dbc473b005
DevMedix <rawlingsmichael01@gmail.com>
2023-04-17 14:23:37 +0000

    Modified content

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,7 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
