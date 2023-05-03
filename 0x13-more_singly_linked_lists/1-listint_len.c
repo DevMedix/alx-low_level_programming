12b8e5df6e0e760aca94b0ca0cbc2553107ffe34
DevMedix <rawlingsmichael01@gmail.com>
2023-05-03 04:59:45 +0000

    content

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	    unsigned int count = 0;
+	unsigned int count = 0;
 
-		while (h != NULL)
-		{
-			count++;
-			h = h->next;
-		}
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
 
 	return (count);
 }
