93e86cbc131d0217d38d7dccdaf6f601fdfa26da
DevMedix <rawlingsmichael01@gmail.com>
2023-07-10 07:48:50 -0700

    modified the file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,37 +8,10 @@
  * Return: Returns 1 on success, -1 on failure.
  */
 
-/*int append_text_to_file(const char *filename, char *text_content)
-{
-	int file;
-	size_t length;
-	ssize_t write_to;
-
-	if (filename == NULL)
-		return (-1);
-
-	if (text_content == NULL)
-		return (-1);
-
-	file = open(filename, O_WRONLY | O_APPEND);
-	if (file == -1)
-		return (-1);
-
-	length = strlen(text_content);
-	write_to = write(file, text_content, length);
-	close(file);
-
-	if (write_to < 0 || (size_t)write_to != length)
-		return (-1);
-
-	return (1);
-}*/
-
-#include <stdio.h>
-
 int append_text_to_file(const char *filename, char *text_content)
 {
 	FILE *file;
+
 	if (filename == NULL)
 		return (-1);
 
