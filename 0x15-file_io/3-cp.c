7be01c32f4873e18982f4f71194af9171c597988
DevMedix <rawlingsmichael01@gmail.com>
2023-07-10 17:25:13 +0000

    a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,82 +1,69 @@
-#include "main.h"
-#define BUFFER_SIZE 1024
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-void copyFile(char *fileFrom, char *fileTo);
-int openFile(char *filename, int flags, int exitCode);
-void handle_error(char *message, int exitCode);
-
+void close_fd_from_to(int fd);
 /**
  * main - entry point
- *
  * @argc: argument count
  * @argv: argument vector
+ *
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
+	char buffer[1024];
+	int fd_from, fd_to, n;
+
 	if (argc != 3)
-		handle_error("Usage: cp file_from file_to", 97);
-	copyFile(argv[1], argv[2]);
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to file %s\n", argv[2]);
+		exit(99);
+	}
+	while ((n = read(fd_from, buffer, 1024)) > 0)
+	{
+		write(fd_to, buffer, n);
+	}
+	if (n == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(fd_from) == -1)
+	{
+		close_fd_from_to(fd_from);
+	}
+	if (close(fd_to) == -1)
+	{
+		close_fd_from_to(fd_to);
+	}
 	return (0);
 }
 
 /**
- * handle_error - Handles an error by printing the error
- * message and exiting with the given exit code.
+ * close_fd_from_to - closes the argument in the terminal
+ * @fd: parameter
  *
- * @message: The error message to print.
- * @exitCode: The exit code to use when exiting the program.
  * Return: void
  */
-void handle_error(char *message, int exitCode)
+void close_fd_from_to(int fd)
 {
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(exitCode);
-}
-
-/**
- * openFile - Opens a file with the specified flags
- * and returns the file descriptor.
- *
- * @filename: The name of the file to open.
- * @flags: The flags to use for opening the file.
- * @exitCode: The exit code to use if opening the file fails.
- * Return: The file descriptor of the opened file.
- */
-int openFile(char *filename, int flags, int exitCode)
-{
-	int file = open(filename, flags);
-
-	if (file == -1)
-		handle_error(filename, exitCode);
-	return (file);
-}
-
-/**
- * copyFile - Copies the content from one file to another file.
- *
- * @fileFrom: The name of the source file.
- * @fileTo: The name of the destination file.
- * Return: void
- */
-void copyFile(char *fileFrom, char *fileTo)
-{
-	int sourceFile = openFile(fileFrom, O_RDONLY, 98);
-	int destinationFile = openFile(fileTo, O_WRONLY | O_CREAT | O_TRUNC, 99);
-
-	char buffer[BUFFER_SIZE];
-	ssize_t bytesRead, bytesWritten;
-
-	while ((bytesRead = read(sourceFile, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytesWritten = write(destinationFile, buffer, bytesRead);
-		if (bytesWritten == -1)
-			handle_error(fileTo, 99);
-	}
-
-	if (bytesRead == -1)
-		handle_error(fileFrom, 98);
-
-	close(sourceFile);
-	close(destinationFile);
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
 }
